370a3f2a833bc4fead1d13734ba0ad67e5158129
jakehl <jake@twilsoft.uk>
2023-07-17 01:40:14 +0100

    non static docs

--- docs.sh
+++ docs.sh
@@ -2,6 +2,5 @@
 swift package --allow-writing-to-directory "./docs" \
   generate-documentation --target "SwiftWebview" \
   --disable-indexing \
-  --transform-for-static-hosting \
   --hosting-base-path "SwiftWebview" \
   --output-path "./docs"
