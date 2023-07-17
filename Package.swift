5a90449dacea80943a1c1717940d5e69a5724ebb
jakehl <jake@twilsoft.uk>
2023-07-17 01:46:19 +0100

    uses jazzy docs

--- Package.swift
+++ Package.swift
@@ -9,9 +9,6 @@ let package = Package(
             targets: ["SwiftWebview"]
         ),
     ],
-    dependencies: [
-        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
-    ],
     targets: [
         .target(
             name: "cWebview",
--- docs.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-swift package --allow-writing-to-directory "./docs" \
-  generate-documentation --target "SwiftWebview" \
-  --disable-indexing \
-  --hosting-base-path "SwiftWebview" \
-  --output-path "./docs"
